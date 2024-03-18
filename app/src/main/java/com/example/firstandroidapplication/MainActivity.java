f9dca53dddd1899bcdce1430ed3463b0c90537da
Dejavu <leon_ye@mail.ru>
2024-03-18 15:34:34 +0300

    first

---
+++ app/src/main/java/com/example/firstandroidapplication/MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.firstandroidapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    // поля
+    private TextView output;
+    private Button button;
+    private int count = 0; // счётчик подсчёта количества микрофлоры
+    private int preCount = 0; // вспомогательный счётчик количества предыдущего поколения количества микрофлоры
+
+    // сохранение данных
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("microflora", count); // запечатывание данных в контейнер Bundle outState под ключом microflora
+        outState.putInt("microfloraPre", preCount); // запечатывание данных в контейнер Bundle outState под ключом microfloraPre
+    }
+
+    // извлечение данных
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        count = savedInstanceState.getInt("microflora"); // извлечение данных из контейнера Bundle savedInstanceState по ключу microflora
+        preCount = savedInstanceState.getInt("microfloraPre"); // извлечение данных из контейнера Bundle savedInstanceState по ключу microfloraPre
+        output.setText("" + count); // вывод на экран извлечённых данных
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "Старт активности", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "Активность доступна для взаимодействия", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "Активность не доступна для взаимодействия", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "Активность остановлена и пропала с экрана", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "Активность уничтожена", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "Активность создана", Toast.LENGTH_SHORT).show();
+
+        // привязка полей к разметке
+        output = findViewById(R.id.output);
+        button = findViewById(R.id.button);
+
+        // обработка нажатия кнопки
+        button.setOnClickListener(listener);
+    }
+
+    // создание слушателя
+    private View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            countFibonacci(); // исполнение одного цикла роста популяции
+            output.setText("" + count); // вывод результатов на экран
+        }
+    };
+
+    //  метод вычисления следующего поколения микрофлоры
+    private int countFibonacci() {
+        if(count == 0 && preCount == 0) { // условие начала отсчёта
+            count = 1; preCount = 0; // появление первой бактерии
+        } if (count == 1 && preCount == 0) { // условие первого поколения
+            count = 1; preCount = 1; // деление бактерии на две
+        } else { // в следующих поколениях происходит деление в соответствии с принципом последовательности ряда Фибоначчи
+            int pre = count; // буферное сохранение счётчика популяции предыдущего поколения
+            count = count + preCount; // подсчёт счётчика популяции текущего поколения
+            preCount = pre; // переинициализации счётчика популяции предыдущего поколения
+        }
+        return count;
+    }
+}
